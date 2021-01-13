97bf1eff43e2be1094d452184723d5a6ff984d9c
qikushu <45323386+qikushu@users.noreply.github.com>
2021-01-13 20:45:11 +0900

    Update postPipeline.sh

--- postPipeline.sh
+++ postPipeline.sh
@@ -1,13 +1,15 @@
 
-PARENT1="tomohiro"
-PARENT2="shoujusei"
+INPUTVCF=$1
+PARENT1=$2
+PARENT2=$3
 LBIMPUTE="$HOME/git/LB-Impute/LB-Impute.jar"
 PIPELINE="$HOME/tassel-5-standalone/run_pipeline.pl"
+GBSTOOLDIR="$HOME/git/gbs"
 
-less $1 | perl prepareLBimpute.pl > new.vcf
+less ${INPUTVCF} | perl $GBSTOOLDIR/prepareLBimpute.pl > new.vcf
 java -Xmx2g -jar $LBIMPUTE -method impute -offspringimpute -f new.vcf -recombdist 20000000 -window 5 -o test.vcf -parents $PARENT1,$PARENT2
 
 perl $PIPELINE -Xmx5g -fork1 -vcf test.vcf -export imputed -exportType Hapmap -runfork1
 
-less imputed.hmp.txt | perl hmp_to_rqtl_cp.pl imputed.hmp.txt $PARENT1 $PARENT2 OUT
+less imputed.hmp.txt | perl $GBSTOOLDIR/hmp_to_rqtl_cp.pl imputed.hmp.txt $PARENT1 $PARENT2 OUT
 
